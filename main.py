da108005999e941da933fe9d0c83ade6dba82331
Aziz Mamasaliev <aziko.mz@gmail.com>
2021-02-19 17:26:52 +0300

    ui

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from kivy.core.window import Window
 from kivy.app import runTouchApp
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+# import lxml
 
 
 class SideBar(GridLayout):
@@ -32,7 +34,8 @@ class TopNavBar(FloatLayout):
         self.add_widget(SideBar())
 
     def hide_menu_action(self):             # FIX ME ! NOT WORKING
-        '''shuld close sideBar:
+        '''
+        should close sideBar:
         '''
         self.remove_widget(SideBar())
 
@@ -41,17 +44,17 @@ class TaskMenu(BoxLayout):   # LEVEL THAT MUST BE SET PROPER: WORKING ON THIS TO
     '''
 
     '''
-    # def scrolling_ef(self):
-    # layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-    # #     # Make sure the height is such that there is something to scroll.
-    # layout.bind(minimum_height=layout.setter('height'))
-    # for i in range(15):3
-    #     btn = Button(text=str(i), size_hint_y=None, height=40)
-    #     layout.add_widget(btn)
-    # #
-    # ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
-    # # #     return root
-    pass
+    def scrolling_ef(self):
+        layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+    #     # Make sure the height is such that there is something to scroll.
+        layout.bind(minimum_height=layout.setter('height'))
+    for i in range(15):3
+        btn = Button(text=str(i), size_hint_y=None, height=40)
+        layout.add_widget(btn)
+    #
+        ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        return root
+        #pass
 
 
 class Controller(FloatLayout):
@@ -65,3 +68,4 @@ class ControllerApp(App):
 
 if __name__ == '__main__':
     ControllerApp().run()
+    print(Label.texture_size[1])
